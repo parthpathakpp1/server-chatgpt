8e3e1802119d269cd7cfbd95fba7a266b44bad5d
parthpathakpp1 <85991449+parthpathakpp1@users.noreply.github.com>
2023-04-05 14:24:26 +0530

    iNITIA commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import env from 'dotenv'
+import { Configuration, OpenAIApi } from 'openai'
+
+const app = express()
+
+env.config()
+
+app.use(cors())
+app.use(bodyParser.json())
+
+// Configure open api
+const configuration = new Configuration({
+    organization: "org-ciCHobJQfyiJtvPolZ8U2edI",
+    apiKey: process.env.API_KEY // VISIT .env AND MAKE CHANGES
+})
+const openai = new OpenAIApi(configuration)
+
+app.get("/", (req, res) => {
+    res.send("Hello World!")
+})
+
+
+app.listen("3080", () => console.log("listening on port 3080"))
+
+//post route for making requests
+app.post('/', async (req, res) => {
+    const { message } = req.body
+
+    try {
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: `${message}`,
+            max_tokens: 100,
+            temperature: .5
+        })
+        res.json({ message: response.data.choices[0].text })
+
+    } catch (e) {
+        console.log(e)
+        res.send(e).status(400)
+    }
+})
